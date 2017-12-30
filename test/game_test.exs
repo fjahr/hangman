aab1bf959dcaf6ad4003b0a4afba8d59706c37c8
Fabian Jahr <fabian.jahr@whu.edu>
2017-12-30 17:52:13 +0100

    implement new game method to start game

---
+++ test/game_test.exs
@@ -0,0 +1,14 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert Enum.all?(game.letters, fn l -> Regex.match?(~r/[a-z]/, l) end)
+  end
+end
