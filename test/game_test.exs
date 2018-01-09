c73cf742c24b09f551775d781a0605d67b139760
Fabian Jahr <fabian.jahr@whu.edu>
2018-01-09 17:01:00 +0100

    letters can not be reused

--- test/game_test.exs
+++ test/game_test.exs
@@ -20,4 +20,18 @@ defmodule GameTest do
     end
   end
 
+  test "first occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of letter is already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
 end
