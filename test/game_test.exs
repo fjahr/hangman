d6206cff3e63bc15f7a1f6009c91f4d341ab5261
Fabian Jahr <fabian.jahr@whu.edu>
2018-01-10 01:13:39 +0100

    lost games and bad guesses are recognized

--- test/game_test.exs
+++ test/game_test.exs
@@ -34,4 +34,39 @@ defmodule GameTest do
     assert game.game_state == :already_used
   end
 
+  test "a good guess is recognized" do
+    game = Game.new_game("test")
+    { game, _tally } = Game.make_move(game, "t")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guess wins the game" do
+    game = Game.new_game("test")
+    { game, _tally } = Game.make_move(game, "t")
+    { game, _tally } = Game.make_move(game, "e")
+    { game, _tally } = Game.make_move(game, "s")
+    { game, _tally } = Game.make_move(game, "t")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("test")
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    game = Game.new_game("test")
+    { game, _tally } = Game.make_move(game, "a")
+    { game, _tally } = Game.make_move(game, "b")
+    { game, _tally } = Game.make_move(game, "c")
+    { game, _tally } = Game.make_move(game, "d")
+    { game, _tally } = Game.make_move(game, "f")
+    { game, _tally } = Game.make_move(game, "g")
+    { game, _tally } = Game.make_move(game, "h")
+    assert game.game_state == :lost
+  end
 end
